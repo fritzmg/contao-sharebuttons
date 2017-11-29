e6bd64ae088a4ad428756d2cedacd171b522e7e6
Fritz Michael Gschwantner <email@spikx.net>
2017-11-29 10:12:04 +0100

    Update tl_news_archive.php

--- system/modules/sharebuttons/dca/tl_news_archive.php
+++ system/modules/sharebuttons/dca/tl_news_archive.php
@@ -5,7 +5,7 @@
  *
  * simple extension to provide a share buttons module
  * 
- * @copyright inspiredminds 2014
+ * @copyright inspiredminds 2014-2017
  * @package   sharebuttons
  * @link      http://www.inspiredminds.at
  * @author    Fritz Michael Gschwantner <fmg@inspiredminds.at>
@@ -22,28 +22,28 @@ $GLOBALS['TL_DCA']['tl_news_archive']['palettes']['default'] .= ';{sharebuttons_
  * Add fields to tl_news_archive
  */
 $GLOBALS['TL_DCA']['tl_news_archive']['fields']['sharebuttons_networks'] = array(
-	'label'                   => &$GLOBALS['TL_LANG']['sharebuttons']['sharebuttons_networks'],
-	'exclude'                 => true,
-	'inputType'               => 'checkbox',
-	'options_callback'		  => array('ShareButtons','getNetworks'),
-	'eval'                    => array('multiple'=>true,'tl_class'=>'w50'),
-	'sql'                     => "blob NULL"
+    'label'            => &$GLOBALS['TL_LANG']['sharebuttons']['sharebuttons_networks'],
+    'exclude'          => true,
+    'inputType'        => 'checkbox',
+    'options_callback' => array('ShareButtons','getNetworks'),
+    'eval'             => array('multiple'=>true,'tl_class'=>'w50', 'tl_style'=>'height:auto;'),
+    'sql'              => "blob NULL"
 );
 
 $GLOBALS['TL_DCA']['tl_news_archive']['fields']['sharebuttons_theme'] = array(
-	'label'                 => &$GLOBALS['TL_LANG']['sharebuttons']['sharebuttons_theme'],
-	'exclude'				=> true,
-	'inputType'				=> 'select',
-	'options_callback'		=> array('ShareButtons','getButtonThemes'),
-	'eval'                  => array('tl_class'=>'w50'),
-	'sql'                   => "varchar(32) NOT NULL default ''"
+    'label'            => &$GLOBALS['TL_LANG']['sharebuttons']['sharebuttons_theme'],
+    'exclude'          => true,
+    'inputType'        => 'select',
+    'options_callback' => array('ShareButtons','getButtonThemes'),
+    'eval'             => array('tl_class'=>'w50'),
+    'sql'              => "varchar(32) NOT NULL default ''"
 );
 
 $GLOBALS['TL_DCA']['tl_news_archive']['fields']['sharebuttons_template'] = array(
-	'label'                 => &$GLOBALS['TL_LANG']['sharebuttons']['sharebuttons_template'],
-	'exclude'				=> true,
-	'inputType'				=> 'select',
-	'options_callback'		=> array('ShareButtons','getSharebuttonsTemplates'),
-	'eval'                  => array('tl_class'=>'w50'),
-	'sql'                   => "varchar(64) NOT NULL default ''"
+    'label'            => &$GLOBALS['TL_LANG']['sharebuttons']['sharebuttons_template'],
+    'exclude'          => true,
+    'inputType'        => 'select',
+    'options_callback' => array('ShareButtons','getSharebuttonsTemplates'),
+    'eval'             => array('tl_class'=>'w50'),
+    'sql'              => "varchar(64) NOT NULL default ''"
 );
